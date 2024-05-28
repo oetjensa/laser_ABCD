b5cecd6d4cb2aca1dfd9e796481232b25dc49174
Annika Oetjens <54310884+oetjensa@users.noreply.github.com>
2024-05-29 09:20:25 +1000

    Update Beam Propagation.py

--- Beam Propagation.py
+++ Beam Propagation.py
@@ -1,13 +1,29 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+path = 'path_to_files'
+# might be necessary to run the folowing:
+#import sys 
+#import os
+#sys.path.append(os.path.abspath(path))
+
 import ABCD_utensils as gb
 
-step = 1
-length = 1000
+# following four lines are only for nicer plots, not necessary
+#import seaborn as sns
+#from pandas.plotting import register_matplotlib_converters
+#register_matplotlib_converters()
+#sns.set_style("darkgrid")
 
+
+step = 1
+# set the total length of the path (in mm)
+length = 17000 # 17 m
+
+# read in laser parameter, position of lenses
 lam, w_0, M2, z_0, rad1, theta = gb.read_param()
 lenses = gb.read_lenses()
-mirrors = gb.mirrors
+mirrors = np.array([])
 
 z_r = (np.pi*w_0**2) / (lam*M2)   # value will be given
 
@@ -26,6 +42,7 @@ Optics, position = gb.Optics(step, length, lenses)
 ABCD, distance, beam_radius, beam_angle, radius_inv = gb.Gauss_beam(q1,Optics, lam, M2, rad1, theta, position)
 
 
-gb.plot(ABCD, distance, beam_radius, radius_inv, lenses, mirrors, save = False)
+gb.plot(ABCD, distance, beam_radius, radius_inv, lenses, mirrors, length, save = False)
 
+# make sure beam radius does not exceed lens/mirror radius
 print('Maximum beam radius: ',max(beam_radius))
